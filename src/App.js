44e22e604e6b44ddba468f2ce87aad6fb4bd971d
Troy Dorman <troydorman@gmail.com>
2021-09-14 16:22:47 -0600

    set up react router with routes and links for our Navbar to work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import Staff from "./components/pages/Staff";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/staff" exact={true} component={Staff} />
+      </div>
+    </Router>
   );
 }
 
