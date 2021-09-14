ad599a60e3ed1947e79e58526511f98df8577a1e
Troy Dorman <troydorman@gmail.com>
2021-09-14 16:58:31 -0600

    implemented Okta Security component to protect routes to utilize their login/auth feature

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import Staff from "./components/pages/Staff";
+import Login from "./components/auth/Login";
+
+function onAuthRequired({ history }) {
+  history.push("/login");
+}
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/staff" exact={true} component={Staff} />
+      <Security
+        issuer="https://dev-39714079.okta.com/oauth2/default"
+        client_id="0oa1t7afnnifYsWs15d7"
+        redirect_uri={window.location.origin + "/implicit/callback"}
+        onAuthRequired={onAuthRequired}
+      >
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Route path="/" exact={true} component={Home} />
+            <SecureRoute path="/staff" exact={true} component={Staff} />
+            <Route
+              path="/login"
+              render={() => <Login baseUrl="https://dev-39714079.okta.com" />}
+            />
+            <Route path="/implicit/callback" component={ImplicitCallback} />
+          </div>
         </div>
-      </div>
+      </Security>
     </Router>
   );
 }
